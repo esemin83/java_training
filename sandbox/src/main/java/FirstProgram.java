20ddead3a654b55c9e1d85579341574d99e4f9c8
esemin83 <esemin83@gmail.com>
2018-05-05 17:07:12 +0300

    Создан пакет для первой программы

--- sandbox/src/main/java/FirstProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstProgram{
-	
-	public static void main(String[] agrs){
-		
-		System.out.println("bonjour");
-		}
-}
---
+++ sandbox/src/main/java/ru/stqa/train/sanbox/FirstProgram1.java
@@ -0,0 +1,8 @@
+package ru.stqa.train.sanbox;
+
+public class FirstProgram1 {
+	
+	public static void main(String[] args){
+		System.out.println("bonjour");
+		}
+}
