e1ecd0e161275f6c8f68e660323b7c9de2eccbaf
esemin83 <esemin83@gmail.com>
2018-05-05 20:18:27 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/train/sanbox/FirstProgram.java
+++ sandbox/src/main/java/ru/stqa/train/sanbox/FirstProgram.java
@@ -3,6 +3,31 @@ package ru.stqa.train.sanbox;
 public class FirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("bonjour");
+    String value = "bonjour";
+    ptr(value);
+
+    //System.out.println(sum(4,8));
+    double l = sum(4, 8);
+    System.out.println(l);
+
+    double a = 1;
+    double b = 5;
+    System.out.println("Сумма " + a + " + " + b + " = " + sum(a, b));
+
+    double c = 5;
+    System.out.println("Квадрат " + c + " = " + sum(c));
+
+  }
+
+  public static void ptr(String val) {
+    System.out.println("Print some value " + "'" + val + "'");
+  }
+
+  public static double sum(double a, double b) {
+    return a + b;
+  }
+
+  public static double sum(double c) {
+    return c * c;
   }
 }
