f242ba061fadfc78a5febf72e15b2c409e48025d
esemin83 <esemin83@gmail.com>
2018-05-05 18:59:41 +0300

    Изменены настройки форматирования

---
+++ sandbox/src/main/java/ru/stqa/train/sanbox/FirstProgram.java
@@ -0,0 +1,8 @@
+package ru.stqa.train.sanbox;
+
+public class FirstProgram {
+
+  public static void main(String[] args) {
+    System.out.println("bonjour");
+  }
+}
--- sandbox/src/main/java/ru/stqa/train/sanbox/FirstProgram1.java
+++
@@ -1,8 +0,0 @@
-package ru.stqa.train.sanbox;
-
-public class FirstProgram1 {
-	
-	public static void main(String[] args){
-		System.out.println("bonjour");
-		}
-}
