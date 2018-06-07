cc76b40315aa344389162df14e52553b293710eb
esemin83 <esemin83@gmail.com>
2018-06-07 23:38:06 +0300

    Лекция 6.2. Генератор тестовых данных, запись данных в файл Страница

---
+++ addressbook-web-tests/src/test/java/ru/stqa/train/addressbook/generators/GroupDataGenerator.java
@@ -0,0 +1,40 @@
+package ru.stqa.train.addressbook.generators;
+
+import com.sun.media.sound.WaveFileWriter;
+import ru.stqa.train.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupDataGenerator {
+
+  public static void main(String... args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<GroupData> groups = generateGroups(count);
+    save(groups, file);
+  }
+
+  private static void save(List<GroupData> groups, File file) throws IOException {
+    Writer writer = new FileWriter(file);
+    for (GroupData group: groups){
+      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
+    }
+    writer.close();
+  }
+
+  private static List<GroupData> generateGroups(int count) {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    for (int i = 0; i < count; i++) {
+      groups.add(new GroupData().withName(String.format("group name %s", i))
+              .withHeader(String.format("group header %s", i))
+              .withFooter(String.format("group footer %s", i)));
+    }
+    return groups;
+  }
+}
