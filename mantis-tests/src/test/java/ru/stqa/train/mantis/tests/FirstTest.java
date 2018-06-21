881f089339e2e23572a6a0a4f5ed248ae5366a38
esemin83 <esemin83@gmail.com>
2018-06-21 21:33:32 +0300

    Добавлен новый модуль mantis

---
+++ mantis-tests/src/test/java/ru/stqa/train/mantis/tests/FirstTest.java
@@ -0,0 +1,12 @@
+package ru.stqa.train.mantis.tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTest extends TestBase {
+
+  @Test
+  public void firstTest() {
+    System.out.print("!");
+  }
+
+}
