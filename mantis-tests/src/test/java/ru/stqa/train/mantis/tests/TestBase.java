1f4207ebc4dc7009aa134d66cc9ccda7ee26aaaf
esemin83 <esemin83@gmail.com>
2018-06-24 16:31:49 +0300

    Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/train/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/train/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.train.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
 
   protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -12,10 +15,14 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(
+            new File("src\\test\\resources\\config_inc.php"),
+            "config_inc.php", "config_inc.php.back");
   }
 
   @AfterSuite
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.back", "config_inc.php");
     app.stop();
   }
 
